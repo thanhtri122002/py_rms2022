f76da5029c3ee481a430356d5a3448e4fd90c1bc
hnq-jan8 <nqhien.tm02@outlook.com>
2022-04-23 18:57:38 +0700

    update input_num(): . added validating input (now user cannot enter number that below than 1)

--- input.py
+++ input.py
@@ -2,6 +2,9 @@ def input_num(str):     # Input a quantity of something
     while True:
         try:
             num = int(input(f'\nEnter number of {str}: '))
+            while num < 1:
+                print('(!) Invalid input')
+                num = int(input(f'\nEnter number of {str}: '))
             return num
         except ValueError:
             print('(!) Invalid input')
