c4ea385cb2bbc0d1625877bb82a3613bc4ea2d8d
hnq-jan8 <nqhien.tm02@outlook.com>
2022-04-28 10:44:41 +0700

    add Search Bar

---
+++ search_bar_demo.py
@@ -0,0 +1,55 @@
+from tkinter import *
+
+def click():
+    entered_text=textbox.get().lower() # this will collect the text from the text entry box
+    output.delete(0.0, END)
+    # try:
+    #     definition = menu[entered_text]
+    # except:
+    #     definition = 'Cannot find that word in the list lma0'
+    result = []
+
+    try:
+        for i in menu:
+            if entered_text in i.lower():
+                result.append(i)
+    except:
+        result.append('Cannot find that word in the list lma0')
+
+    for i in result:
+        output.insert(END, i + '\n')
+    # output.insert(END, result)
+
+# main
+window = Tk()
+window.title('Restautant Management Application')
+window.configure(background='white')
+
+# label
+Label(window, text='Thầy Sơn ăn gì', font='none 12 bold').grid(row=0, column=0, sticky=W)
+
+# text entry box
+textbox = Entry(window, width=20, bg='grey', fg='white')
+textbox.grid(row=1, column=0, sticky=W)
+
+# submit button
+Button(window, text='Oge!', width=6, command=click).grid(row=2, column=0, sticky=W)
+
+# label another text box but output
+Label(window, text='\nRéult', bg='white', font='none 12 bold').grid(row=3, column=0, sticky=W)
+
+# text output box
+output = Text(window, width=50, height=6, wrap=WORD, background='lightgrey')
+output.grid(row=5, column=0, columnspan=2, sticky=W)
+
+# menu
+menu = ['Phở bòa tái', 'Phở gà', 'Phở các thứ', 'Phở bòa chín']
+
+# exit label
+Label(window, text='Ấn ấn cái nút này để cái này này trở về nhà nhà', bg='white', font='none 9').grid(row=6, column=0, sticky=W)
+
+# exit button
+Button(window, text='Back', width=6, command=window.destroy, font='none 9').grid(row=7, column=0, sticky=W)
+
+# main
+window.mainloop()
